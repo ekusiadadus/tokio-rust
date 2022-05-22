a972cbb6fd2d5feaad0280eb88d3ed051ead895a
ekusiadadus <ekusiadadus@gmail.com>
2022-05-22 16:36:51 +0900

    fix: envfile:

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
+#[allow(dead_code)]
 use mini_redis::{client, Result};
+use std::env;
+
+fn get_env(name: &str) -> String {
+    env::var(name).expect(name)
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    // let redis_server = get_env("REDIS_SERVER");
     // Open a connection to the mini-redis address.
     let mut client = client::connect("127.0.0.1:6379").await?;
 
