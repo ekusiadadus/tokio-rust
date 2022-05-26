1cb82a3341da72e25482bf2a0123a51c4811700e
ekusiadadus <ekusiadadus@gmail.com>
2022-05-26 21:44:46 +0900

    feat:server

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,8 @@
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
-};
-
-#[tokio::main]
-async fn main() {
-    let listner = TcpListener::bind("localhost:8000").await.unwrap();
-    let (mut socket, _addr) = listner.accept().await.unwrap();
-    loop {
-        let mut buffer = [0u8; 1024];
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+use std::net::TcpListener;
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection established!");
     }
 }
